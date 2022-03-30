0a78cb871cd77e215e67f4025eb2d3bb334c14c3
傅东海 <296322762@qq.com>
2022-03-30 15:04:35 +0800

    update

--- agora-app-server/src/main/java/com/easemob/agora/AgoraServerApplication.java
+++ agora-app-server/src/main/java/com/easemob/agora/AgoraServerApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import java.net.URI;
+
 /**
  * ClassName: SpringBootApplication
  * description:
@@ -15,6 +17,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class AgoraServerApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(AgoraServerApplication.class, args);
+
+
+
     }
 }
